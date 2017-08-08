45151a9c1b371f45fa560aadf5bcac57ca60e045
Lasse Alm <lassealm2@gmail.com>
2017-08-08 20:29:24 +0200

    initial commit

---
+++ DataHandler/Handlers/TradeFileFileHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataHandler.Handlers
+{
+    public class TradeFileFileHandler : IFileHandler
+    {
+        public string FilePath { get; set; }
+
+        public TradeFileFileHandler(string filePath)
+        {
+            FilePath = filePath;
+        }
+    }
+}
